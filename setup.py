ab0c94552c11af7fa4236c74b29d2e6987ed19a3
kronenthaler <kronenthaler@gmail.com>
2016-01-30 18:42:53 +0100

    refactor: rename test files to automate coverage

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+# Inspired by the example at https://pytest.org/latest/goodpractises.html
+class NoseTestCommand(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Run nose ensuring that argv simulates running nosetests directly
+        import nose
+        nose.run_exit(argv=['nosetests','-w','tests'])
 
 
 setup(name='SwiftFormat',
@@ -7,4 +21,7 @@ setup(name='SwiftFormat',
       url="http://github.com/kronenthaler/swift-format",
       version='1.3',
       license='BSD License',
-      packages=find_packages(exclude=['tests']))
+#      packages=find_packages(exclude=['tests']),
+      setup_requires=['nose', 'coverage'],
+      cmdclass={'test': NoseTestCommand},
+)
